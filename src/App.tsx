0be5f4ab4346e013e8f7428acfef288c718be9bf
TonmoyZohani <tonmoyzohani@gmail.com>
2025-07-06 14:58:20 +0600

    state implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Component from "./starter/02-props";
+import Component from "./starter/03-state";
 
 function App() {
   return (
     <main>
       {/* <Component name='Tonmoy' id={1821348}/> */}
-      <Component name='Tonmoy' id={1821348}>
-           <p>This is a child element.</p>
+      <Component name="Tonmoy" id={1821348}>
+        <p>This is a child element.</p>
         <button>Click Me</button>
       </Component>
     </main>
