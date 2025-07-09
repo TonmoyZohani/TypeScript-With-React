3dcec2450608e72adcf49d0d60d422c21d2f2bb1
TonmoyZohani <tonmoyzohani@gmail.com>
2025-07-09 22:07:11 +0600

    fetch tours function created

--- src/starter/08-fetch-data/types.ts
+++ src/starter/08-fetch-data/types.ts
@@ -1,6 +1,6 @@
-import { z } from 'zod';
-import axios from 'axios';
-const url = 'https://course-api.com/react-tours-project';
+import { z } from "zod";
+import axios from "axios";
+const url = "https://course-api.com/react-tours-project";
 
 export const tourSchema = z.object({
   id: z.string(),
@@ -13,20 +13,12 @@ export const tourSchema = z.object({
 
 export type Tour = z.infer<typeof tourSchema>;
 
-export const fetchTours = async ():Promise<Tour[]> =>{
-    const response = await axios.get<Tour[]>(url);
-    const result = tourSchema.array().safeParse(response.data);
-    if(!result.success){
-        throw new Error('Parsing Failed')
-    }
-    return result.data
-}
+export const fetchTours = async (): Promise<Tour[]> => {
+  const response = await axios.get<Tour[]>(url);
+  const result = tourSchema.array().safeParse(response.data);
+  if (!result.success) {
+    throw new Error("Parsing Failed");
+  }
+  return result.data;
+};
 
-// export const fetchTours = async (): Promise<Tour[]> => {
-//   const response = await axios.get<Tour[]>(url);
-//   const result = tourSchema.array().safeParse(response.data);
-//   if (!result.success) {
-//     throw new Error('Parsing failed');
-//   }
-//   return result.data;
-// };
