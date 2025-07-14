863e3d0df9951ee59dadf98fbd722ff3b2053fe8
TonmoyZohani <tonmoyzohani@gmail.com>
2025-07-14 21:52:37 +0600

    counter state set

--- src/starter/09-rtk/counterSlice.ts
+++ src/starter/09-rtk/counterSlice.ts
@@ -13,4 +13,18 @@ const initialState: CounterState = {
   status: "pending",
 };
 
-
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
