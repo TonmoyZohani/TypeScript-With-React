7f3d58ac87aafa4883f3583b695decee57b84766
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-08 05:13:41 +0600

    object done

--- src/basics/objects.ts
+++ src/basics/objects.ts
@@ -1,9 +1,26 @@
-const cricketer: { name: string; age: number } = { name: "Tonmoy", age: 25 };
-console.log(cricketer);
+// const cricketer: { readonly type: "Player"; name: string; age: number } = {
+//   type: "Player",
+//   name: "Tonmoy",
+//   age: 25,
+// };
+// console.log(cricketer);
 
-const user: { firstName: string; middleName?: string; lastName: string } = {
-  firstName: "Tonmoy",
-  lastName: "Zohani",
+type Player = {
+  type: "Player";
+  name: string;
+  age: number;
 };
 
-console.log(user)
+const player: Player = {
+  type: "Player",
+  name: "Tonmoy",
+  age: 25,
+};
+
+console.log(player);
+
+const playerDetails = (cricketer: Player, heighest: number): string => {
+  return `${cricketer.name} is ${cricketer.age} years old.His best is ${heighest} meters.`;
+};
+
+console.log(playerDetails(player, 123));
