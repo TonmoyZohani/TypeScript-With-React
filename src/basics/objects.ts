480500091e3c86ac96a890de749748b71fc1a625
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-08 05:00:55 +0600

    updated

---
+++ src/basics/objects.ts
@@ -0,0 +1,9 @@
+const cricketer: { name: string; age: number } = { name: "Tonmoy", age: 25 };
+console.log(cricketer);
+
+const user: { firstName: string; middleName?: string; lastName: string } = {
+  firstName: "Tonmoy",
+  lastName: "Zohani",
+};
+
+console.log(user)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-import "./basics/variables";
+// import "./basics/variables";
+import "./basics/objects";
